7230bde8565e8402a3ec14547574ed98e69e9778
namanb <namanbansal1101@gmail.com>
2025-10-05 17:31:20 +0530

    remove streaming

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -5,5 +5,4 @@ import { generateVideo, uploadVideoToYouTube } from "@/lib/inngest-functions";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [generateVideo, uploadVideoToYouTube],
-  streaming: "force",
 });
