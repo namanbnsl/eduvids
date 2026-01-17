beb848c2bf07560286a54ba757acce264370be40
namanb <namanbansal1101@gmail.com>
2026-01-17 14:23:57 +0530

    posthog proxy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/ph/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ph/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
